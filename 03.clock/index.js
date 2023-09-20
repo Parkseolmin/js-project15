749c687b2f2df390ccae3534bc4a940e166c40e0
Parkseolmin <qkrtjfals2@naver.com>
2023-09-20 14:02:49 +0900

    🥳 js 프로젝트

--- 03.clock/index.js
+++ 03.clock/index.js
@@ -5,9 +5,9 @@ const sec = document.querySelector('.sec');
 function clock() {
   const now = new Date();
 
-  hour.innerText = now.getHours();
-  min.innerText = now.getMinutes();
-  sec.innerText = now.getSeconds();
+  hour.innerText = now.getHours().toString().padStart(2, '0');
+  min.innerText = now.getMinutes().toString().padStart(2, '0');
+  sec.innerText = now.getSeconds().toString().padStart(2, '0');
 }
 
 setInterval(clock, 1000);
--- 12.trello-app/index.js
+++ 12.trello-app/index.js
@@ -1,4 +1,73 @@
+// 💡 https://github.com/erumcoding/js-101-part-1
+
 const form = document.querySelector('form');
+const blocks = document.querySelectorAll('.list');
+
+let from, to;
+
+let todoList = [],
+  doingList = [],
+  doneList = [];
+
+const lists = {
+  todo: todoList,
+  doing: doingList,
+  done: doneList,
+};
+
+const removeTodo = (event) => {
+  event.preventDefault();
+
+  const { id } = event.target;
+  const { id: listId } = event.target.parentElement;
+
+  event.target.remove();
+  lists[listId] = lists[listId].filter((todo) => {
+    return todo.id !== id;
+  });
+
+  saveList(listId);
+};
+
+const saveList = (listId) => {
+  localStorage.setItem(listId, JSON.stringify(lists[listId]));
+};
+
+const dragEnd = (event) => {
+  const { id } = event.target;
+
+  if (from === to) {
+    return;
+  }
+
+  event.target.remove();
+  lists[from] = lists[from].filter((todo) => {
+    if (todo.id !== id) {
+      return todo;
+    } else {
+      createElement(to, todo);
+    }
+  });
+
+  saveList(from);
+  saveList(to);
+};
+
+const dragOver = (event) => {
+  event.preventDefault();
+
+  const { id: targetId } = event.target;
+  const listIds = Object.keys(lists);
+
+  if (listIds.includes(targetId)) {
+    to = targetId;
+  }
+};
+
+const dragStart = (event) => {
+  from = event.target.parentElement.id;
+  to = from;
+};
 
 const createElement = (listId, todo) => {
   const list = document.querySelector(`#${listId}`);
@@ -7,9 +76,14 @@ const createElement = (listId, todo) => {
   item.id = todo.id;
   item.innerText = todo.text;
   item.className = 'item';
-  item.draggable = true;
+  item.draggable = 'true';
 
-  list.append(item);
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+  item.addEventListener('contextmenu', removeTodo);
+
+  list.appendChild(item);
+  lists[listId].push(todo);
 };
 
 const createTodo = (event) => {
@@ -18,8 +92,6 @@ const createTodo = (event) => {
   const input = document.querySelector('input');
   const id = uuidv4();
 
-  // UUID
-
   const newTodo = {
     id,
     text: input.value,
@@ -27,6 +99,36 @@ const createTodo = (event) => {
 
   createElement('todo', newTodo);
   input.value = '';
+  saveList('todo');
 };
 
+const loadList = () => {
+  const userTodoList = JSON.parse(localStorage.getItem('todo'));
+  const userDoingList = JSON.parse(localStorage.getItem('doing'));
+  const userDoneList = JSON.parse(localStorage.getItem('done'));
+
+  if (userTodoList) {
+    userTodoList.forEach((todo) => {
+      createElement('todo', todo);
+    });
+  }
+
+  if (userDoingList) {
+    userDoingList.forEach((todo) => {
+      createElement('doing', todo);
+    });
+  }
+
+  if (userDoneList) {
+    userDoneList.forEach((todo) => {
+      createElement('done', todo);
+    });
+  }
+};
+
+loadList();
+
 form.addEventListener('submit', createTodo);
+blocks.forEach((block) => {
+  block.addEventListener('dragover', dragOver);
+});
